268ca9d9f1f51afb0ed0dc881d522a746f28053b
ruialves <rui@kinsu.co.uk>
2019-03-11 15:10:25 +0000

    Adding phone search using #

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -28,7 +28,8 @@ export class HomeComponent implements OnInit {
 
   buttonText: string;
 
-  areaConfig: any;
+  mentionConfig: any;
+
 
   constructor(
     private usersData: UsersService,
@@ -48,6 +49,22 @@ export class HomeComponent implements OnInit {
       this.users = data;
       console.log(this.users)
     })
+
+    this.mentionConfig = {
+      mentions: [
+        {
+          items: this.users ,
+          triggerChar: '@',
+          labelKey: 'username'
+        },
+        {
+          items: this.users ,
+          triggerChar: '#',
+          labelKey: 'phone'
+        }
+      ]
+    }
+
 
   }
 
@@ -90,64 +107,5 @@ export class HomeComponent implements OnInit {
     console.log('TOBEIMPLEMENTED')
   }
 
-  searchUser: string
-  isSearchMode:boolean
-
-  postValueChange(event){
-
-    if(event.key === '@') {
-      this.isSearchMode = true
-    }
-    else {
-
-
-    }
-
-  }
-
-  setupAreaConfig () {
-    console.log('list')
-    this.areaConfig = {
-      dropdown: [{
-        // trigger can be a string or a regexp
-        // in this case this regexp should match Twitter-style @mentions
-        trigger: /@([A-Za-z]+[_A-Za-z0-9]+)/gi,
-        // The list() function gets called when the trigger matches
-        // it should return a list of elements to be suggested
-        list: function(match: any[], callback: (arg0: any) => void){
-          // match is the regexp return, in this case it returns
-          // [0] the full match, [1] the first capture group => username
-
-          console.log('list')
-          this.usersData.searchUser(match[1])
-            .subscribe(data => {
-            this.users = data;
-            console.log(this.users)
-          }).success(function(data){
-            // Prepare the fake data
-            var listData = data.map(function(element){
-              return {
-                display: element.userName, // This gets displayed in the dropdown
-                item: element // This will get passed to onSelect
-              };
-            });
-            callback(listData);
-          }).error(function(err){
-            console.error(err);
-          });
-        },
-        // Function that gets called when an item is selected
-        // it's return value gets added to the textarea
-        onSelect: function(item){
-          return item.userName;
-        },
-        // mode can be append or replace
-        // here we want replace because the user has already typed
-        // part of the username
-        mode: 'replace'
-      }
-      ]
-    }
-  }
 
 }
